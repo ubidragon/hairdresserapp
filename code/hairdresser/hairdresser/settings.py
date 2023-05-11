cf1c8977c77159eb39697714a2119cea97b5267e
Ubidragon <ubidragon@protonmail.com>
2023-05-11 19:28:46 +0200

    refactor del cambio de modulo de gestion

--- code/hairdresser/hairdresser/settings.py
+++ code/hairdresser/hairdresser/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hairdresserapp',
-    'hairdresserapp_gestionCitas',
+    'gestion',
+    'acceso',
 ]
 
 MIDDLEWARE = [
