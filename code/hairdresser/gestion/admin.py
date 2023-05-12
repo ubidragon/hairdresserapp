be7ec43ccc4f196a44b2c745dff44f549ced9625
Ubidragon <ubidragon@protonmail.com>
2023-05-12 13:34:34 +0200

    Deshabilito panel de admin

--- code/hairdresser/gestion/admin.py
+++ code/hairdresser/gestion/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models  import Cita, Oferta, Roles, Servicio, Ubicacion, Usuario
+
 # Register your models here.
+class UsuarioAdmin(admin.ModelAdmin):
+    readonly_fields=("created", "updated")
+    
+
+class RolAdmin(admin.ModelAdmin):
+    readonly_fields=("created", "updated")  
+
+
+class CitaAdmin(admin.ModelAdmin):
+    pass 
+      
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Roles, RolAdmin)
+admin.site.register(Cita, CitaAdmin)
