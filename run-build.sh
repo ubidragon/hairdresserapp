1f7c5f13c3deb7ae2bc8bd37fd70173e0721953a
Ubidragon <ubidragon@protonmail.com>
2023-05-15 20:30:44 +0200

    Update version

--- run-build.sh
+++ run-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker image rm -f hairdresserapp:0.2
-docker build --no-cache -t hairdresserapp:0.2 .
+docker image rm -f hairdresserapp:1.0
+docker build --no-cache -t hairdresserapp:1.0 .
